b62b300b97107c887bbdcfc28c50d97b3e8cf890
Shane Holloway <shane.holloway@ieee.org>
2020-04-17 09:04:14 -0600

    Fixed main/module export matching generated files

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,12 @@
+import pkg from './package.json'
 import rpi_jsy from 'rollup-plugin-jsy'
 
-const configs = []
-export default configs
+export default {
+  input: `code/index.jsy`,
+  plugins: [rpi_jsy()],
+  external: ['dgram', 'path'],
+  output: [
+    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.main, format: 'cjs', exports:'default', sourcemap: true },
+  ]}
 
-const sourcemap = true
-const external = ['dgram', 'path']
-const plugins = [rpi_jsy()]
-
-add_jsy('index')
-
-
-function add_jsy(src_name) {
-  configs.push({
-    input: `code/${src_name}.jsy`, plugins, external,
-    output: [
-      { file: `esm/${src_name}.mjs`, format: 'es', sourcemap },
-      { file: `cjs/${src_name}.cjs`, format: 'cjs', exports:'default', sourcemap },
-    ]}) }
